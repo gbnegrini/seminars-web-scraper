187d702b16fe1be6c1b89d66c3c4f0c4638e57df
gbnegrini <negrini.guilherme@gmail.com>
2018-12-19 13:09:12 -0200

    Main updated

--- SeminarsScraperTool.py
+++ SeminarsScraperTool.py
@@ -1,9 +1,13 @@
 from Log import Log
 from seminars_web_scraper import WebScraper
+from API_GoogleCal import GoogleCalAPI
 
 log = Log()
 log.create()
 scraper = WebScraper(log)
 url = 'https://www.ufrgs.br/ppgbioq/category/eventos/'
 scraper.get_html(url)
+seminars_list = scraper.get_data()
+google_cal_api = GoogleCalAPI(log)
+google_cal_api.create_events(seminars_list)
 log.close()
